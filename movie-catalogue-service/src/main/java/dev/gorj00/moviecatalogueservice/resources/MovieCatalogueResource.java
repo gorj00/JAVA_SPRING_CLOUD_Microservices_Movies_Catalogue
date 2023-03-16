964a122959c17f31d7f1d4589be2dcc46710fb4e
Jan Gorol <os.jangorol@gmail.com>
2023-03-16 20:33:21 +0100

    basic rest template

--- movie-catalogue-service/src/main/java/dev/gorj00/moviecatalogueservice/resources/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/dev/gorj00/moviecatalogueservice/resources/MovieCatalogueResource.java
@@ -1,12 +1,17 @@
 package dev.gorj00.moviecatalogueservice.resources;
 
 import dev.gorj00.moviecatalogueservice.models.CatalogueItem;
+import dev.gorj00.moviecatalogueservice.models.Movie;
+import dev.gorj00.moviecatalogueservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalogue")
@@ -14,8 +19,24 @@ public class MovieCatalogueResource {
 
     @RequestMapping("{/userId}")
     public List<CatalogueItem> getCatalogue(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogueItem("Transformers", "test", 4)
-        );
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 5)
+                );
+        return ratings.stream().map(rating -> {
+                    // for each movie ID, call movie info service and get details
+                    // creates object from json response and model
+                    Movie movie = restTemplate.getForObject("http://localhost" +
+                                    ":8082/movies/" + rating.getMovieId(),
+                            Movie.class);
+                    // put them all together
+                    return new CatalogueItem(movie.getName(),
+                            "Test", rating.getRating());
+                })
+                .collect(Collectors.toList());
     }
 }
